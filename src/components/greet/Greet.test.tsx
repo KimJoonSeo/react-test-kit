3a9b49102da1351d70e3fb4d3563e7ffe8111854
KimJoonSeo <junjun901225@gmail.com>
2023-10-02 19:59:06 +0900

    test: add a test case to test rendering name property in Greet

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -6,4 +6,9 @@ describe('Greet', () => {
         const textElement = screen.getByText(/Hello Guest/)
         expect(textElement).toBeInTheDocument()
     })
+    test('renders correctly with name property', () => {
+        render(<Greet name={'vishow'}/>);
+        const textElement = screen.getByText(/Hello vishow/)
+        expect(textElement).toBeInTheDocument()
+    })
 })
