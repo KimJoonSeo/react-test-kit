24048790e8ff0215b0020e4909ec86a4dbce2fd4
KimJoonSeo <junjun901225@gmail.com>
2023-10-08 23:35:47 +0900

    test: add a code for testing user interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import {Application} from "./components/application/Application";
+import { Counter } from './components/counter/counter';
 
 function App() {
   return (
     <div className="App">
-      <Application />
+      <Counter />
     </div>
   );
 }
