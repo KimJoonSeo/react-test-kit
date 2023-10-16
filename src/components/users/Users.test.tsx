ac27143bef640f2beb70b1ca20a285d236f26cfc
KimJoonSeo <junjun901225@gmail.com>
2023-10-16 22:24:53 +0900

    test: add a error handling test code using msw

--- src/components/users/Users.test.tsx
+++ src/components/users/Users.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import { Users } from './Users'
+import {server} from "../../mocks/server";
+import {rest} from "msw";
 
 
 describe('Users', () => {
@@ -13,4 +15,17 @@ describe('Users', () => {
         const users = await screen.findAllByRole('listitem')
         expect(users).toHaveLength(3)
     });
+    test('renders error', async () => {
+        server.use(
+            rest.get(
+                'https://jsonplaceholder.typicode.com/users',
+                (req, res, ctx) => {
+                    return res(ctx.status(500))
+                }
+            )
+        )
+        render(<Users />)
+        const error = await screen.findByText('Error fetching users')
+        expect(error).toBeInTheDocument()
+    })
 });
